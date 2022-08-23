f5090d3e7aedf6483c0244337d433df563627094
Rafael de Paula <xspynks@gmail.com>
2022-08-23 01:08:30 -0300

    primeiro commit

---
+++ app/controllers/api/v1/artists_controller.rb
@@ -0,0 +1,49 @@
+module Api
+  module V1
+    class ArtistsController < ApplicationController
+      before_action :authorize_access_request!, except: [:show, :index]
+      before_action :set_artist, only: [:show, :update, :destroy]
+
+      def index
+        @artists = Artist.all
+
+        render json: @artists
+      end
+
+      def show
+        render json @artist
+      end
+
+      def create
+        @artist = Artist.new(artist_params)
+        
+        if @artist.save
+          render json: @artist, status: :created
+        else
+          render json: @artist.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @artist.update(artist_params)
+          render json: @artist
+        else
+          render json: @artist.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @artist.destroy
+      end
+
+      private
+      def set_artist
+        @artist = Artist.find(params[:id])
+      end
+
+      def artist_params 
+        params.require(:artist).permit(:name, :user_id)
+      end
+    end
+  end
+end
